033f622cf2e62fd1d382e977caee1464272fd592
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-12-03 18:55:23 +0100

    Create designer.routes.js

---
+++ src/v1/routes/designer.routes.js
@@ -0,0 +1,45 @@
+import express from "express";
+import methodNotAllowed from "../../middlewares/methodNotAllowed.js";
+import { isAuth } from "../../middlewares/auth.js";
+import { productValidator } from "../validators/product.validator.js";
+import { checkDesignerRegistration } from "../../middlewares/designerRegistration.js";
+import {
+  createProduct,
+  updateProduct,
+  getProducts,
+  getProductById,
+  getProductsByDesigner,
+  deleteProduct,
+} from "../controllers/product.controller.js";
+import { checkProductOwnership } from "../../middlewares/productOwnership.js";
+
+const router = express.Router();
+
+router
+  .route("/product")
+  .get(getProducts)
+  .post(isAuth, checkDesignerRegistration, productValidator, createProduct)
+  .all(methodNotAllowed);
+router
+  .route("/product/:designerId")
+  .get(isAuth, getProductsByDesigner)
+  .all(methodNotAllowed);
+router
+  .route("/product/:productId")
+  .get(getProductById)
+  .patch(
+    isAuth,
+    checkDesignerRegistration,
+    checkProductOwnership,
+    productValidator,
+    updateProduct
+  )
+  .delete(
+    isAuth,
+    checkDesignerRegistration,
+    checkProductOwnership,
+    deleteProduct
+  )
+  .all(methodNotAllowed);
+
+export default router;
