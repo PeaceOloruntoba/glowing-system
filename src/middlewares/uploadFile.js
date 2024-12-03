0e64c1c954455973233b3cb25b0d7fcda1828a53
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-12-03 17:11:21 +0100

    Create uploadFile.js

---
+++ src/middlewares/uploadFile.js
@@ -0,0 +1,13 @@
+import cloudinary from "../../config/cloudinary.js";
+
+const uploadImagesToCloudinary = async (images) => {
+  const uploadPromises = images.map((image) =>
+    cloudinary.uploader.upload(image, {
+      folder: `${cloudinary.config().default_folder}/products`,
+    })
+  );
+  const uploadedImages = await Promise.all(uploadPromises);
+  return uploadedImages.map((file) => file.secure_url);
+};
+
+export default uploadImagesToCloudinary;
