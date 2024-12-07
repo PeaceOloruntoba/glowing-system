f6bcbf7bd8846449e984af4347f5c54600a5d6b8
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-12-07 12:34:19 +0100

    Create client.model.js

---
+++ src/v1/models/client.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const measurementSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  { _id: false }
+);
+
+const clientSchema = new Schema(
+  {
+    designerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clientName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    measurements: {
+      type: [measurementSchema],
+      required: true,
+      validate: {
+        validator: (arr) => Array.isArray(arr) && arr.length > 0,
+        message: "Measurements must include at least one object.",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Client", clientSchema);
