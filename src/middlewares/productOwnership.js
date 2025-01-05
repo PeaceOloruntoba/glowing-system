e13c0d30061cffbd41185622d2b43a4d260fe762
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-01-05 08:06:54 +0100

    ggg

--- src/middlewares/productOwnership.js
+++ src/middlewares/productOwnership.js
@@ -1,5 +1,6 @@
 import Product from "../v1/models/product.model.js";
 import ApiError from "../utils/apiError.js";
+import Client from "../v1/models/client.model.js";
 
 export const checkProductOwnership = async (req, res, next) => {
   const { userId } = req.user;
@@ -16,6 +17,30 @@ export const checkProductOwnership = async (req, res, next) => {
       return res.status(403).json({
         success: false,
         message: "You are not authorized to update this product",
+      });
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const checkClientOwnership = async (req, res, next) => {
+  const { userId } = req.user;
+  const { clientId } = req.params;
+
+  try {
+    const client = await Client.findById(clientId);
+
+    if (!client) {
+      throw ApiError.notFound("Product not found");
+    }
+
+    if (client.designerId.toString() !== userId) {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized to update this client",
       });
     }
 
