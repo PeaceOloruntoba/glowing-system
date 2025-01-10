2ad7a4a515efb44a51c350891b8464cc8eaeb149
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-01-10 12:49:02 +0100

    Create booking.model.js

---
+++ src/v1/models/booking.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const UserProfileSchema = new Schema(
+  {
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: [String],
+      enum: [
+        "pending",
+        "ongoing",
+        "paid",
+        "packaged",
+        "dispatched",
+        "delivered",
+        "cancelled",
+      ],
+      default: ["pending"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("UserProfile", UserProfileSchema);
