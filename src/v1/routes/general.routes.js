c64c09d3ee55f2234a83d6af73b628fe407ba150
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-01-18 11:19:06 +0100

    Update general.routes.js

--- src/v1/routes/general.routes.js
+++ src/v1/routes/general.routes.js
@@ -10,3 +10,5 @@ const router = express.Router();
 router.route("/products").get(getProducts).all(methodNotAllowed);
 
 router.route("/products/:productId").get(getProductById).all(methodNotAllowed);
+
+export default router
