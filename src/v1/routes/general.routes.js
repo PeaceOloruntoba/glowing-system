7474c77f65e84c3996f95f2f388700c5dcc53f6c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-01-21 17:50:58 +0100

    Update general.routes.js

--- src/v1/routes/general.routes.js
+++ src/v1/routes/general.routes.js
@@ -4,10 +4,13 @@ import {
   getProducts,
   getProductById,
 } from "../controllers/general/product.controller.js";
+import { getDesignerById, getDesigners } from "../controllers/general/designer.controller.js";
 
 const router = express.Router();
 
 router.route("/products").get(getProducts).all(methodNotAllowed);
 router.route("/products/:productId").get(getProductById).all(methodNotAllowed);
+router.route("/designers").get(getDesigners).all(methodNotAllowed);
+router.route("/designers/:designerId").get(getDesignerById).all(methodNotAllowed);
 
 export default router
