3fee1d2158f727a6ee0fb0bf638c1f8676efdd74
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-01-22 15:31:58 +0100

    Update designer.service.js

--- src/v1/services/general/designer.service.js
+++ src/v1/services/general/designer.service.js
@@ -1,18 +1,39 @@
 import ApiError from "../../../utils/apiError.js";
 import DesignerProfile from "../../models/designerProfile.model.js";
+import UserProfile from "../../models/userProfile.model.js";
 import Product from "../../models/product.model.js";
 
 export default {
   getAllDesigners: async function () {
-    const designers = await DesignerProfile.find()
+    const result = await DesignerProfile.find();
+    // Map over bookings to fetch additional user details
+    const designers = await Promise.all(
+      result.map(async (designer) => {
+        const userProfile = await UserProfile.findOne({
+          userId: designer.userId,
+        });
+        const fullName = userProfile?.fullName;
+        return {
+          ...designer.toObject(),
+          fullName,
+        };
+      })
+    );
     return designers;
   },
 
   getDesignerById: async function (designerId) {
-    const designer = await DesignerProfile.findById(designerId);
-    if (!designer) {
-      throw ApiError.notFound("Designer not found");
-    }
+    const result = await DesignerProfile.findById(designerId);
+    const userProfile = await UserProfile.findOne({ userId: result.userId });
+    const fullName = userProfile?.fullName;
+    const phoneNumber = userProfile?.phoneNumber;
+    const email = userProfile?.email;
+    const designer = {
+      ...result.toObject(),
+      fullName,
+      email,
+      phoneNumber,
+    };
     return designer;
   },
 };
