ceb132455bf4e8c751a1c8e22a65d7394fa8ccc6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-01-23 14:37:17 +0100

    Create cart.service.js

---
+++ src/v1/services/user/cart.service.js
@@ -0,0 +1,71 @@
+import Cart from "../../models/"
+const createCart = async (user) => {
+  const cart = new Cart({ user });
+  return await cart.save();
+};
+
+const getCartByUser = async (userId) => {
+  return await Cart.findOne({ user: userId }).populate("products.product");
+};
+
+const addItemToCart = async (userId, productId) => {
+  const cart = await Cart.findOne({ user: userId });
+
+  if (!cart) {
+    throw new Error("Cart not found");
+  }
+
+  const productIndex = cart.products.findIndex(
+    (item) => item.product.toString() === productId
+  );
+
+  if (productIndex !== -1) {
+    cart.products[productIndex].quantity += 1;
+  } else {
+    cart.products.push({ product: productId, quantity: 1 });
+  }
+
+  return await cart.save();
+};
+
+const updateCartItemQuantity = async (userId, productId, quantity) => {
+  const cart = await Cart.findOne({ user: userId });
+
+  if (!cart) {
+    throw new Error("Cart not found");
+  }
+
+  const productIndex = cart.products.findIndex(
+    (item) => item.product.toString() === productId
+  );
+
+  if (productIndex === -1) {
+    throw new Error("Product not found in cart");
+  }
+
+  cart.products[productIndex].quantity = quantity;
+
+  return await cart.save();
+};
+
+const removeItemFromCart = async (userId, productId) => {
+  const cart = await Cart.findOne({ user: userId });
+
+  if (!cart) {
+    throw new Error("Cart not found");
+  }
+
+  cart.products = cart.products.filter(
+    (item) => item.product.toString() !== productId
+  );
+
+  return await cart.save();
+};
+
+module.exports = {
+  createCart,
+  getCartByUser,
+  addItemToCart,
+  updateCartItemQuantity,
+  removeItemFromCart,
+};
