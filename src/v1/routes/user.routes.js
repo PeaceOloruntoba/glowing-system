0f0ceec3713a355b98febd0f3e33258143a53ff6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-01-24 04:07:45 +0100

    Update user.routes.js

--- src/v1/routes/user.routes.js
+++ src/v1/routes/user.routes.js
@@ -6,6 +6,13 @@ import {
   getAllBookings,
   getBookingById,
 } from "../controllers/user/booking.controller.js";
+import {
+  addToCart,
+  decrementQuantity,
+  getCart,
+  incrementQuantity,
+  removeFromCart,
+} from "../controllers/user/cart.controller.js";
 
 const router = express.Router();
 
@@ -18,6 +25,23 @@ router
   .route("/booking/:bookingId")
   .get(isAuth, getBookingById)
   .patch(isAuth)
+  .all(methodNotAllowed);
+router
+  .route("/cart")
+  .get(isAuth, getCart)
+  .post(isAuth, addToCart)
+  .all(methodNotAllowed);
+router
+  .route("/cart/:productId")
+  .delete(isAuth, removeFromCart)
+  .all(methodNotAllowed);
+router
+  .route("/cart/:productId/increment")
+  .patch(isAuth, incrementQuantity)
+  .all(methodNotAllowed);
+router
+  .route("/cart/:productId/decrement")
+  .patch(isAuth, decrementQuantity)
   .all(methodNotAllowed);
 
 export default router;
