4f4f6b0352cce133b04f4eef4b054f0e45239ae0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-01-24 04:01:08 +0100

    Update cart.model.js

--- src/v1/models/cart.model.js
+++ src/v1/models/cart.model.js
@@ -1,25 +1,56 @@
 import mongoose from "mongoose";
+const { Schema } = mongoose;
 
-const cartSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  products: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        default: 1,
-        required: true,
-      },
+const cartSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-  ],
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+          default: 1,
+        },
+        totalPrice: {
+          type: Number,
+          required: true,
+          default: 0,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+// Update totalPrice and totalAmount before saving
+cartSchema.pre("save", async function (next) {
+  let totalAmount = 0;
+
+  for (const item of this.items) {
+    const product = await mongoose.model("Product").findById(item.productId);
+    if (product) {
+      item.totalPrice = product.ourPrice * item.quantity;
+      totalAmount += item.totalPrice;
+    }
+  }
+
+  this.totalAmount = totalAmount;
+  next();
 });
 
-module.exports = mongoose.model("Cart", cartSchema);
+export default mongoose.model("Cart", cartSchema);
