72c40bd95ed4b0c32c02da5795472db7593110a8
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-31 16:40:51 +0100

    Update designerSubscription.js

--- src/middlewares/designerSubscription.js
+++ src/middlewares/designerSubscription.js
@@ -1,10 +1,11 @@
 import DesignerProfile from "../v1/models/designerProfile.model.js";
 
 const checkSubscription = async (req, res, next) => {
+  console.log(req.user)
   try {
-    if (req.user.roles.includes("designer")) {
+    if (req.user) {
       const designerProfile = await DesignerProfile.findOne({
-        userId: req.user.id,
+        userId: req.user.userId,
       });
 
       if (!designerProfile || !designerProfile.subActive) {
